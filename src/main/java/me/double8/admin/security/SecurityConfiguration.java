a5aa493f8657bbc3666ab2e4d2bb3a14abd1a088
Jung Min Seo <minseo124@pubple.com>
2021-11-08 12:22:22 +0900

    security configure 수정

--- src/main/java/me/double8/admin/security/SecurityConfiguration.java
+++ src/main/java/me/double8/admin/security/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package me.double8.admin.security;
 
+import me.double8.admin.member.domain.Member;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,9 +23,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().antMatchers("/admin/**").permitAll()
-                .and()
-                .formLogin()
-                .permitAll();
+                .authorizeRequests((request) ->
+                        request.antMatchers("/").permitAll()
+                                .anyRequest().authenticated()
+                );
+        http.formLogin();
+        http.httpBasic();
     }
 }
